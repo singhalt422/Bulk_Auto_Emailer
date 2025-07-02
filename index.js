395ccea5ca5fe5c4f46b1d6b44176f359b790908
singhalt422 <singhalt422@gmail.com>
2025-07-02 18:19:50 +0530

    update 02072025

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
