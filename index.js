0f1e9301a88da2d190cc5874c7cb82b18c972496
singhalt422 <singhalt422@gmail.com>
2025-07-02 15:06:35 +0530

    third

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 // Create Express app
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set up body parser for handling form data
 app.use(bodyParser.urlencoded({ extended: true }));
